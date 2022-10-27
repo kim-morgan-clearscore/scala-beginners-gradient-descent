65ccfeb0d8642ad82ba98c87df5c27788962da76
Kim Morgan <kimberley.morgan@clearscore.com>
2022-10-27 15:17:30 +0100

    last week's work

--- code/src/main/scala/gradientdescent/numerical/Animation.scala
+++ code/src/main/scala/gradientdescent/numerical/Animation.scala
@@ -1,50 +1,50 @@
-/*
- * Copyright 2022 Creative Scala
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// /*
+//  * Copyright 2022 Creative Scala
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *     http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
 
-package gradientdescent.numerical
+// package gradientdescent.numerical
 
-import doodle.core._
-import doodle.syntax.all._
-import doodle.interact.animation._
-import doodle.interact.syntax.all._
-import doodle.java2d._
-import cats.effect.unsafe.implicits.global
+// import doodle.core._
+// import doodle.syntax.all._
+// import doodle.interact.animation._
+// import doodle.interact.syntax.all._
+// import doodle.java2d._
+// import cats.effect.unsafe.implicits.global
 
-import gradientdescent.Plot
+// import gradientdescent.Plot
 
-object Animation {
-  val data = gradientdescent.Data.sineData
-  val f: (Double, Double) => Double = (x, a) => a * Math.sin(x)
-  val scale = Plot.linearInterpolation(50.0, 1.0)
-  val loss: Double => Double =
-    ??? // Create the loss function here, given f and data above
+// object Animation {
+//   val data = gradientdescent.Data.sineData
+//   val f: (Double, Double) => Double = (x, a) => a * Math.sin(x)
+//   val scale = Plot.linearInterpolation(50.0, 1.0)
+//   val loss: Double => Double =
+//     ??? // Create the loss function here, given f and data above
 
-  def animation(initial: Double, iterations: Int): Transducer[Picture[Unit]] = {
-    Transducer
-      .fromList(List.range(0, iterations))
-      .scanLeft(initial) { (a, _) =>
-        val updatedX = GradientDescent.iterate(a)(loss)
-        updatedX
-      }
-      .map { a =>
-        Plot
-          .data(scale)(data)
-          .on(Plot.function(-6.0, 6.0, scale)(x => f(x, a)))
-      }
-  }
+//   def animation(initial: Double, iterations: Int): Transducer[Picture[Unit]] = {
+//     Transducer
+//       .fromList(List.range(0, iterations))
+//       .scanLeft(initial) { (a, _) =>
+//         val updatedX = GradientDescent.iterate(a)(loss)
+//         updatedX
+//       }
+//       .map { a =>
+//         Plot
+//           .data(scale)(data)
+//           .on(Plot.function(-6.0, 6.0, scale)(x => f(x, a)))
+//       }
+//   }
 
-  animation(10.0, 1000).animate(Frame.size(600, 600))
-}
+//   animation(10.0, 1000).animate(Frame.size(600, 600))
+// }
--- code/src/main/scala/gradientdescent/symbolic/Expression.scala
+++ code/src/main/scala/gradientdescent/symbolic/Expression.scala
@@ -14,24 +14,106 @@
  * limitations under the License.
  */
 
+// We're going to represent expressions as follows. An Expression is one of:
+
+// A literal number, containing a Double value.
+// A variable, containing a String name, which represents a variable such as  or  in our function.
+// The addition of two Expressions.
+// The multiplication of two Expressions.
+
 package gradientdescent.symbolic
 
 sealed trait Expression {
-  def +(that: Expression): Expression = ???
+  import Expression._
 
-  def *(that: Expression): Expression = ???
+  def +(that: Expression): Expression = 
+    Addition(this, that)
 
-  def bind(variableName: String, value: Double): Expression = ???
+  def *(that: Expression): Expression = 
+    Multiplication(this, that)
 
-  def simplify: Expression = ???
+  override def toString(): String =
+    this match
+      case Literal(value) => value.toString()
+      case Variable(name) => name
+      case Addition(operandOne, operandTwo) =>
+        s"(${operandOne.toString()} + ${operandTwo.toString()})"
+      case Multiplication(operandOne, operandTwo) =>
+        s"(${operandOne.toString()} * ${operandTwo.toString()})"
+    
 
-  def differentiate(variableName: String): Expression = ???
+// Implement a method bind that accepts both a variable name (a String) and a value for that variable (a Double),
+//  and substitutes the value for all occurrences of the variable within the Expression.
+
+  def bind(variableName: String, value: Double): Expression = 
+    this match {
+      case Literal(v) => Literal(v)
+      case Variable(name) => 
+        if (name == variableName) Literal(value) else Variable(name)
+      case Addition(operandOne, operandTwo) => 
+        Addition(operandOne.bind(variableName, value), operandTwo.bind(variableName, value))
+      case Multiplication(operandOne, operandTwo) => 
+        Multiplication(operandOne.bind(variableName, value), operandTwo.bind(variableName, value))
+    }
+
+  def simplify: Expression = 
+    this match {
+      case Literal(value) => Literal(value)
+      case Variable(name) => Variable(name)
+      case Addition(operandOne, operandTwo) => 
+        (operandOne.simplify, operandTwo.simplify) match {
+          case (Literal(v1), Literal(v2)) => Literal(v1 + v2)
+          case (Literal(0), o2) => o2
+          case (o1, Literal(0)) => o1
+          case (o1, o2) => Addition(o1, o2)
+        }
+      case Multiplication(operandOne, operandTwo) => 
+        (operandOne.simplify, operandTwo.simplify) match {
+          case (Literal(v1), Literal(v2)) => Literal(v1 * v2)
+          case (Literal(0), _) => Literal(0)
+          case (_, Literal(0)) => Literal(0)          
+          case (Literal(1), expr) => expr
+          case (expr, Literal(1)) => expr
+          case (o1, o2) => Multiplication(o1, o2)
+        }
+    }
+
+  def differentiate(variableName: String): Expression = 
+    this match
+      case Literal(value) => Literal(0)
+      case Variable(name) => if (name == variableName) Literal(1) else Literal(0)
+      case Addition(operandOne, operandTwo) => 
+        operandOne.differentiate(variableName) + operandTwo.differentiate(variableName)
+      case Multiplication(operandOne, operandTwo) => 
+        (operandOne.differentiate(variableName) * operandTwo) + (operandOne * operandTwo.differentiate(variableName))
+    
 }
 object Expression {
+  final case class Literal(value: Double) extends Expression
+  final case class Variable(name: String) extends Expression
+  final case class Addition(operandOne: Expression, operandTwo: Expression) extends Expression
+  final case class Multiplication(operandOne: Expression, operandTwo: Expression) extends Expression
 
-  /** Create a literal given it's value. */
-  def literal(value: Double): Expression = ???
+  /** Create a literal given its value. */
+  def literal(value: Double): Expression = 
+    Literal(value)
 
-  /** Create a variable given it's name. */
-  def variable(name: String): Expression = ???
+  /** Create a variable given its name. */
+  def variable(name: String): Expression = 
+    Variable(name)
+}
+
+@main def expressionExample() = {
+  // 8x + 8
+  val expr1 = Expression.literal(8) * Expression.variable("x") + Expression.literal(8)
+  // 4x^2 + 8x + 16
+  val x = Expression.variable("x")
+  val expr2 = (Expression.literal(4) * x * x) + (Expression.literal(8) * x) + Expression.literal(16)
+  println(expr1)
+  println(expr1.bind("x", 4))
+  println(expr1.bind("x", 4).simplify)
+  println(expr1.bind("x", 4).simplify.simplify)
+  println(expr1.bind("x", 4).simplify.simplify.simplify)
+  println(expr1.differentiate("x").simplify)
+  println(expr2.differentiate("x").simplify.simplify)
 }
